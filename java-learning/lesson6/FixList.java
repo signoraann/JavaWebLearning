3083f18ee9d69d6babb8cd27c53e221231e011cb
signoraann <atumanova796@gmail.com>
2026-02-23 12:33:47 +0300

    Lesson 6 add

---
+++ java-learning/lesson6/FixList.java
@@ -0,0 +1,7 @@
+package lesson6;
+public class FixList {
+	public static void main (String[] args) {
+		String moovies [] = {"Топи", "Лихие", "Метод", "Пророк","Мажор"};
+		System.out.println("The first moovie: " + moovies[0] + "; the third moovie: " + moovies[2]);
+	}
+}
