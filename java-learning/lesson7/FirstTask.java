31ca2687d9af43ccb296e8bfb2a5e8c594e0d77c
signoraann <atumanova796@gmail.com>
2026-02-24 17:38:25 +0300

    First task

---
+++ java-learning/lesson7/FirstTask.java
@@ -0,0 +1,11 @@
+package lesson7;
+
+public class FirstTask {
+	public static void sayHello() {
+		 System.out.println("Hi!");
+	}
+	
+	public static void main(String[] args) {
+		sayHello();
+	}
+}
