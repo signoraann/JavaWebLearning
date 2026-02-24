1eabefd06eb60a180a744beb97ea5b0f7b65e84a
signoraann <atumanova796@gmail.com>
2026-02-24 14:16:47 +0300

    Corrected 'w'

--- java-learning/lesson6/WhileLoop.java
+++ java-learning/lesson6/WhileLoop.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class WhileLoop {
 	public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
-	String w = " ";
+	String word = " ";
 		System.out.println("Input word:");
 	while(!w.equals("exit")) {
 		System.out.println("Type 'exit' to stop");
-		w = scanner.nextLine();
+		word = scanner.nextLine();
 		System.out.println(w);
 		}
 	scanner.close();
