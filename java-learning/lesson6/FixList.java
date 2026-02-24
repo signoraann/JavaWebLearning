ae2635e62e637eeb5e6b5f92ddb54d1d06d84335
signoraann <atumanova796@gmail.com>
2026-02-24 14:12:02 +0300

    Corrected movies

--- java-learning/lesson6/FixList.java
+++ java-learning/lesson6/FixList.java
@@ -1,7 +1,7 @@
 package lesson6;
 public class FixList {
 	public static void main (String[] args) {
-		String moovies [] = {"Топи", "Лихие", "Метод", "Пророк","Мажор"};
-		System.out.println("The first moovie: " + moovies[0] + "; the third moovie: " + moovies[2]);
+		String movies [] = {"Топи", "Лихие", "Метод", "Пророк","Мажор"};
+		System.out.println("The first movie: " + movies[0] + "; the third movie: " + movies[2]);
 	}
 }
