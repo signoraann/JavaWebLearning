92c6b8241180e359fac758164c3d75370350e199
signoraann <atumanova796@gmail.com>
2026-02-26 19:48:51 +0300

    Task 2

---
+++ java-learning/lesson7/SecondTask.java
@@ -0,0 +1,13 @@
+package lesson7;
+
+public class SecondTask {
+	public static void Greeting(String name) {
+		System.out.println("Welcome, " + name + "!");
+	}
+	
+	public static void main(String[] args) {
+		Greeting("Kiryl");
+		Greeting("Ann");
+		Greeting("Nastya");
+	}
+}
