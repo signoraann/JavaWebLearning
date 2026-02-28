dead1bba1536ffffd72cdd2fb9d82fc415d338ce
signoraann <atumanova796@gmail.com>
2026-02-28 23:40:30 +0300

    Fix task four

--- java-learning/lesson7/TaskFour.java
+++ java-learning/lesson7/TaskFour.java
@@ -1,9 +1,6 @@
 package lesson7;
 import java.util.Scanner;
 public class TaskFour {
-	public  static boolean defineSenior(int age) {
-		return age >= 65;
-	}
 	public static void main (String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("How old are you?");	
@@ -11,5 +8,9 @@ public class TaskFour {
 		boolean isSenior = defineSenior(age);
 		if (isSenior) { System.out.println("Senior Citizen");}
 		scanner.close();
+	}
+	
+		public  static boolean defineSenior(int age) {
+		return age >= 65;
 	}
 }
