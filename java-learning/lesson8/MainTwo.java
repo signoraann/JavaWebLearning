4c9a7e09d323898844731d07abf0c97156ec526a
signoraann <atumanova796@gmail.com>
2026-03-11 14:15:48 +0300

    Task 3

--- java-learning/lesson8/MainTwo.java
+++ java-learning/lesson8/MainTwo.java
@@ -4,17 +4,11 @@ import java.util.Scanner;
 public class MainTwo {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Input your dog's breed");
-		String myDogBreed = scanner.nextLine();
-		System.out.println("Input your dog's age");
-		int myDogAge = scanner.nextInt();
-		scanner.nextLine();
-		System.out.println("Input neighbors dog's breed");
-		String neighborsDogBreed = scanner.nextLine();
-		System.out.println ("Input neighbors dog's age");
-		int neighborsDogAge = scanner.nextInt();
-		Dog myDog = new Dog(myDogBreed, myDogAge);
-		Dog neighborsDog = new Dog(neighborsDogBreed, neighborsDogAge);
+		Dog myDog = new Dog("Husky", 3);
+		Dog neighborsDog = new Dog("Doberman", 5);
+		System.out.println("My dog: " + myDog + "; Neighbors dog: " + neighborsDog);
+		neighborsDog.breed = "Bulldog";
+		neighborsDog.age = 3;
 		System.out.println("My dog: " + myDog + "; Neighbors dog: " + neighborsDog);
 		scanner.close();
 	}
