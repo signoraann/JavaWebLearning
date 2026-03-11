daf778c44f639a4c80b6b660254518d4d015c0c8
signoraann <atumanova796@gmail.com>
2026-03-11 14:19:03 +0300

    Fix task 3

--- java-learning/lesson8/MainTwo.java
+++ java-learning/lesson8/MainTwo.java
@@ -1,15 +1,12 @@
  package lesson8;
-import java.util.Scanner;
 
 public class MainTwo {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		Dog myDog = new Dog("Husky", 3);
 		Dog neighborsDog = new Dog("Doberman", 5);
 		System.out.println("My dog: " + myDog + "; Neighbors dog: " + neighborsDog);
 		neighborsDog.breed = "Bulldog";
 		neighborsDog.age = 3;
 		System.out.println("My dog: " + myDog + "; Neighbors dog: " + neighborsDog);
-		scanner.close();
 	}
 }
