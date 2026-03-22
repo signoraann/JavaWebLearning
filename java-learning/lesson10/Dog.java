0555b8314b1974c89df3c464c865cb447cb6f843
signoraann <atumanova796@gmail.com>
2026-03-22 08:26:15 +0300

    Added getters and setters(+warning when age is negative)

--- java-learning/lesson10/Dog.java
+++ java-learning/lesson10/Dog.java
@@ -3,11 +3,29 @@ package lesson10;
 public class Dog {
 	private String name;
 	private int age;
+	
 	public Dog (String name, int age) {
 		this.name = name;
-		this.age = age;
+		if (age<0) {System.out.println("Age cannot be negative!");}
+		else {this.age = age;}
 	} 
-	
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	} 
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		if (age<0) {System.out.println("Age cannot be negative!");}
+		else {this.age = age;}
+	}
+
 	@Override
 	public String toString() {
 		return name + " " + age + " years old";
--- java-learning/lesson10/Main.java
+++ java-learning/lesson10/Main.java
@@ -4,7 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		Dog myDog = new Dog ("Woof", 3);
 		System.out.println(myDog);
-		myDog.name = "Rex";
+		myDog.setName("Rex");
 		System.out.println(myDog);
+		myDog.setAge(-4);
+		System.out.println(myDog);	
 	}
 }
