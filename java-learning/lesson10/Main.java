b0a83df7298d28b251289c44938ff22444db36bb
signoraann <atumanova796@gmail.com>
2026-03-29 13:09:57 +0300

    Typos corrected in Main.java

--- java-learning/lesson10/Main.java
+++ java-learning/lesson10/Main.java
@@ -6,22 +6,22 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		Dog myDog = new Dog ();
 
-		System.out.println("Input your's dog name: ");
+		System.out.println("Input your dog's name: ");
 		String inputName = scanner.nextLine();
 		myDog.setName(inputName);
 	
-		int age = inputAge(scanner, "Input your's dog age:  ");
+		int age = inputAge(scanner, "Input your dog's age:  ");
 		myDog.setAge(age);
 		System.out.println(myDog);	
 
-		int newAge = inputAge(scanner, "Change your's dog age. Input new dog's age: ");
+		int newAge = inputAge(scanner, "Change your dog's age. Input new dog's age: ");
 		myDog.setAge(newAge);
 		System.out.println(myDog);	
 		scanner.close();
 	}
 
-	private static int inputAge(Scanner scanner, String promt) {
-		System.out.println(promt);
+	private static int inputAge(Scanner scanner, String prompt) {
+		System.out.println(prompt);
 		int age = scanner.nextInt();
 		if (age < 0) {
 			System.out.println("Age cannot be negative! Try again :)");
