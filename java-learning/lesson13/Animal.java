582b276f3cda6b3cdca1a80030a4218c3b7733ba
signoraann <atumanova796@gmail.com>
2026-04-15 16:52:18 +0300

    A general class Animal has been created

---
+++ java-learning/lesson13/Animal.java
@@ -0,0 +1,14 @@
+package lesson13;
+
+public class Animal {
+	public String name;
+
+	public Animal (String name) {
+		this.name = name;
+	}
+
+	public void eat() {
+		System.out.println(name + " eating");
+	}
+
+}
