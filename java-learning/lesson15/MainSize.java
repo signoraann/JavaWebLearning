010a40a81b9ff1cfddb29b05f311d5b4d3aebc4f
signoraann <atumanova796@gmail.com>
2026-07-05 16:44:25 +0300

    A constructor and a getPrice() method were added to the Size enum

---
+++ java-learning/lesson15/MainSize.java
@@ -0,0 +1,9 @@
+package lesson15;
+
+public class MainSize {
+   public static void main(String[] args) {
+	System.out.println(Size.LARGE.getPrice());
+	System.out.println(Size.MEDIUM.getPrice());
+	System.out.println(Size.SMALL.getPrice());
+  }
+}
