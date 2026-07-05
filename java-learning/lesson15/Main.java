501c57e32160ce7a4a1b0d1c86c4f1ce0a799f0e
signoraann <atumanova796@gmail.com>
2026-07-05 15:43:46 +0300

    a switch statement with a different message for each status added to the OrderStatus enum

--- java-learning/lesson15/Main.java
+++ java-learning/lesson15/Main.java
@@ -2,7 +2,13 @@ package lesson15;
 
 public class Main {
   public static void main(String[] args) {
-	OrderStatus delivered = OrderStatus.DELIVERED;
-	System.out.println(delivered);
+	OrderStatus status = OrderStatus.DELIVERED;
+	OrderStatus.getStatusMessage(status);
+
+	OrderStatus status1 = OrderStatus.SHIPPED;
+	OrderStatus.getStatusMessage(status1);
+
+	OrderStatus status2 = OrderStatus.PENDING;
+	OrderStatus.getStatusMessage(status2);
   }
 }
--- java-learning/lesson15/OrderStatus.java
+++ java-learning/lesson15/OrderStatus.java
@@ -1,5 +1,14 @@
 package lesson15;
 
 public enum OrderStatus {
-	PENDING, SHIPPED, DELIVERED
+	PENDING, SHIPPED, DELIVERED;
+
+	public static void getStatusMessage(OrderStatus status) {
+	
+	        switch (status) {
+		case PENDING -> System.out.println("Your order is being prepared");
+		case SHIPPED -> System.out.println("Your order is on the way!");
+		case DELIVERED -> System.out.println("Your order has arrived");
+	        }
+	}
 }
