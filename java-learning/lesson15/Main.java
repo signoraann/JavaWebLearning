ffa11ad552f0a79169d9fe963051e467dafdd660
signoraann <atumanova796@gmail.com>
2026-07-06 14:01:34 +0300

    The for-each using in Main.java

--- java-learning/lesson15/Main.java
+++ java-learning/lesson15/Main.java
@@ -2,13 +2,8 @@ package lesson15;
 
 public class Main {
   public static void main(String[] args) {
-	OrderStatus status = OrderStatus.DELIVERED;
+	for (OrderStatus status : OrderStatus.values()) {
 	System.out.println(status.getStatusMessage());
-
-	OrderStatus status1 = OrderStatus.SHIPPED;
-	System.out.println(status1.getStatusMessage());
-
-	OrderStatus status2 = OrderStatus.PENDING;
-	System.out.println(status2.getStatusMessage());
+	}
   }
 }
