6c1883151f49b1aca1331df744de84bf6e968ee9
signoraann <atumanova796@gmail.com>
2026-07-07 17:31:37 +0300

    Double everi number in the numbers list and collect the result into a new list using lambda syntax

---
+++ java-learning/lesson16/LambdaMain.java
@@ -0,0 +1,15 @@
+package lesson16;
+import java.util.ArrayList; 
+
+public class LambdaMain{
+  public static void main(String[] args) {
+	ArrayList<Integer> numbers = new ArrayList<>();
+	for(int num=1; num<=10;num++) {numbers.add(num);}
+	System.out.println(numbers);
+
+	numbers.stream()
+		.map((n) -> n*2)
+		.toList()
+		.forEach(n->System.out.println(n));
+     }
+}
