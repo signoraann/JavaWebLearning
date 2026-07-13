7215fd90fe7eb74dbf80811a84efefd63e15e292
signoraann <atumanova796@gmail.com>
2026-07-13 22:40:46 +0300

    Created the Animal.java class and generated code without typing it (using "Generate")

---
+++ java-learning/lesson18/Animal.java
@@ -0,0 +1,36 @@
+package lesson18;
+
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name,
+                  int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
