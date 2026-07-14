83fa1ad6f6ecc14943bc9b093818a9e46b0583ec
signoraann <atumanova796@gmail.com>
2026-07-14 13:56:06 +0300

    Added the "JavaFaker" dependency in pom.xml to generate a random name in Main.java

---
+++ java-learning/lesson19/lesson19/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import com.github.javafaker.Faker;
+
+public class Main {
+    public static void main (String[] args) {
+        Faker faker = new Faker();
+        System.out.println(faker.name().fullName());
+    }
+}
